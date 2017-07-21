220ea1e49e9b8ae759164229889ad3547e03c58f
streetloner <764611964@qq.com>
2017-07-21 14:51:28 +0800

    我的流程页面搭建完成!

--- src/components/myFlow/leaveFlow.vue
+++ src/components/myFlow/leaveFlow.vue
@@ -19,7 +19,7 @@
 							申请人
 						</div>
 						<div  class="flowTitle2">
-							冯良
+							<input type="text" name="" v-model="applyName">
 						</div>
 					</div>
 					<div class="flowTitle">
@@ -27,7 +27,7 @@
 							标题
 						</div>
 						<div  class="flowTitle2">
-							今天出走,只为明日,能与荣耀同行!
+							<input type="text" name="" v-model="title">
 						</div>
 					</div>
 					<div class="flowStatus">
@@ -35,7 +35,9 @@
 							开始时间
 						</div>
 						<div  class="flowStatus2">
-							2016-09-09 14:30
+							<span class="time" data="beginDate" v-on:click="openPicker($event)">
+								{{ showBeginDate }}
+							</span>
 						</div>
 					</div>
 					<div class="flowStatus">
@@ -43,10 +45,12 @@
 							结束时间
 						</div>
 						<div  class="flowStatus2">
-							2016-09-09 14:30
+							<span class="time" data="endDate" v-on:click="openPicker($event)">
+								{{ showEndDate }}
+							</span>
 						</div>
 					</div>
-					<div class="flowStatus">
+					<!-- <div class="flowStatus">
 						<div  class="flowStatus1">
 							实际开始
 						</div>
@@ -61,13 +65,13 @@
 						<div  class="flowStatus2">
 							打卡
 						</div>
-					</div>
+					</div> -->
 					<div class="flowTitle">
 						<div  class="flowTitle1">
 							请假原因
 						</div>
-						<div  class="flowTitle2">
-							今天出走,只为明日,能与荣耀同行!
+						<div  class="flowTitle2 reason">
+							<textarea type="text" name="" v-model="reason"></textarea> 
 						</div>
 					</div>
 				</div>
@@ -81,6 +85,7 @@
 							<span class="flowPeople">办公室主任</span>
 							<img slot="icon" src="../../static/icon/arrow.svg" width="24" height="24" >
 							<span class="flowPeople">总经理</span>
+							<span class="flowPeople addPeople">添加</span>
 						</div>
 					</div>
 					<div class="flowMould">
@@ -89,32 +94,27 @@
 							<span class="flowPeople">人事主管</span>
 							<span class="flowPeople">办公室主任</span>
 							<span class="flowPeople">总经理</span>
+							<span class="flowPeople addPeople">添加</span>
 						</div>
 					</div>
 				</div>
-				<div class="flowArea">
-					<div class="flowStatus" style="border-top:none;">
-						<div  class="flowStatus1">
-							审批人
-						</div>
-						<div  class="flowStatus2">
-							风声,思念铃
-						</div>
-					</div>
-					<div class="flowStatus">
-						<div  class="flowStatus1">
-							审批意见
-						</div>
-						<div  class="flowStatus2">
-							有点假的不好弄
-						</div>
-					</div>
-					<div class="button reject-btn">
-						驳回
-					</div>
+				<div class="btnArea">
+					<mt-button type="primary" size="large">发起流程</mt-button>
 				</div>
 			</div>
 		</div>
+		<mt-datetime-picker
+			ref="beginPicker"
+			type="datetime"
+			v-model="beginDate"
+			v-on:confirm="selectBeginDate">
+		</mt-datetime-picker>
+		<mt-datetime-picker
+			ref="endPicker"
+			type="datetime"
+			v-model="endDate"
+			v-on:confirm="selectEndDate">
+		</mt-datetime-picker>
 	</div>
 </template>
 
@@ -123,9 +123,31 @@
 export default {
 	data() {
 		return {
+			applyName:'',
+			title:'',
+			reason:'',
+			beginDate:new Date(),
+			showBeginDate:new Date().Format('yyyy-MM-dd hh:mm'),
+			endDate:new Date(),
+			showEndDate:new Date().Format('yyyy-MM-dd hh:mm'),
 		}
 	},
 	methods: {
+		selectBeginDate:function(value){
+			this.showBeginDate = value.Format('yyyy-MM-dd hh:mm');
+		},
+		selectEndDate:function(value){
+			this.showEndDate = value.Format('yyyy-MM-dd hh:mm');
+		},
+		openPicker:function(event){
+			var el = event.currentTarget;
+			var a = $(el).attr('data');
+			if (a == 'beginDate') {
+				this.$refs.beginPicker.open();
+			}else if (a == 'endDate') {
+				this.$refs.endPicker.open();
+			}
+		},
 		toPage:function(event){
 			var el = event.currentTarget;
 			var a = $(el).attr('data');
